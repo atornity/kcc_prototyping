340dc94e2df8ab4778c21b93a2eb857669bc9012
Joona Aalto <jondolf.dev@gmail.com>
2025-05-04 23:11:00 +0300

    Add atmospheric scattering to test scene

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use avian3d::{PhysicsPlugins, prelude::PhysicsDebugPlugin};
-use bevy::prelude::*;
+use bevy::{
+    pbr::{Atmosphere, light_consts::lux},
+    prelude::*,
+    render::camera::Exposure,
+};
 use bevy_enhanced_input::prelude::Actions;
 use input::{DefaultContext, InputPlugin};
 use level::LevelPlugin;
@@ -34,7 +38,14 @@ fn setup(mut commands: Commands) {
         Actions::<DefaultContext>::default(),
         Character::default(),
         children![(
+            // Camera with Earth's atmosphere
             Camera3d::default(),
+            Camera {
+                hdr: true,
+                ..Default::default()
+            },
+            Atmosphere::EARTH,
+            Exposure::SUNLIGHT,
             Projection::Perspective(PerspectiveProjection {
                 fov: 90.0_f32.to_radians(),
                 ..Default::default()
@@ -42,5 +53,15 @@ fn setup(mut commands: Commands) {
             DefaultCamera,
             Transform::from_xyz(0.0, 0.5, 0.0)
         )],
+    ));
+
+    // Sun
+    commands.spawn((
+        DirectionalLight {
+            shadows_enabled: true,
+            illuminance: lux::RAW_SUNLIGHT,
+            ..default()
+        },
+        Transform::from_xyz(0.0, 2.0, 1.0).looking_at(Vec3::ZERO, Vec3::Y),
     ));
 }
