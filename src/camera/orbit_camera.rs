f122633c72d5b2c5711320bf0152e5550eb5a6ae
Philipp Haack <PhilippHaackCode@gmail.com>
2025-05-08 00:10:08 +0200

    fix: orbit zoom target

--- src/camera/orbit_camera.rs
+++ src/camera/orbit_camera.rs
@@ -52,8 +52,8 @@ pub(crate) fn zoom_input(
             let zoom_input = actions.action::<OrbitZoom>().value().as_axis2d();
             let zoom_delta = zoom_input.y * arm.distance * 0.1; // TODO: configurable speed
 
-            arm.distance -= zoom_delta;
-            arm.distance = arm.distance.clamp(0.1, 100.0); // TODO: configurable range
+            arm.target_distance -= zoom_delta;
+            arm.target_distance = arm.target_distance.clamp(0.1, 100.0); // TODO: configurable range
         }
     }
 
