9142af03994491a2dd66f386e96632e0a009e711
poopy <gonesbird@gmail.com>
2025-05-10 00:41:57 +0200

    stari step fix

--- src/movement.rs
+++ src/movement.rs
@@ -214,12 +214,18 @@ fn movement(
                     return true;
                 };
 
-                if !is_walkable(step_hit.normal1, character.up, EXAMPLE_WALKABLE_ANGLE) {
+                if !is_walkable(
+                    step_hit.normal1,
+                    character.up,
+                    // Subtract a small amount from walkable angle to make sure we can't step
+                    // on surfaces that are nearly excactly the walkable angle of the character
+                    EXAMPLE_WALKABLE_ANGLE - 1e-4,
+                ) {
                     return true;
                 }
 
-                // Make sure velocity is not upwards after stepping. This is because if 
-                // we're a capsule, the roundness of it will cause an upward velocity, 
+                // Make sure velocity is not upwards after stepping. This is because if
+                // we're a capsule, the roundness of it will cause an upward velocity,
                 // giving us a launching up effect that we don't want.
                 let up_vel = movement.translation.dot(*character.up).max(0.0);
                 *movement.velocity -= character.up * up_vel;
